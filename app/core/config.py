58be5468655db631322ab3a59dadaf35e79e6fc0
jujumilk3 <jujumilk3@gmail.com>
2023-05-30 00:12:17 +0900

    app init with openai api key

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,16 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class BaseConfigs:
     APP_ROOT_DIR: str = Path(__file__).parent.parent.parent
     APP_NAME: str = "LDD API"
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    OPENAI_ORG_KEy: str = os.getenv("OPENAI_ORG_KEY")
+    OPENAI_ORG_KEY: str = os.getenv("OPENAI_ORG_KEY")
 
 
 configs = BaseConfigs()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,37 @@
+import openai
 from fastapi import FastAPI, status
+from loguru import logger
+from pydantic import BaseModel, Field
+
 from app.core.config import configs
 
 
-app = FastAPI(
-    title=configs.APP_NAME,
-)
+def create_app():
+    # init app
+    _app = FastAPI(
+        title=configs.APP_NAME,
+    )
+
+    try:
+        # init openai api
+        openai.api_key = configs.OPENAI_API_KEY
+        openai.organization = configs.OPENAI_ORG_KEY
+        # it returns error if api key or org is invalid
+        openai.Engine.list()
+    except Exception as e:
+        logger.error(f"OpenAI API init failed: {e}")
+        raise e
+
+    return _app
 
 
-@app.get("/health", status_code=status.HTTP_200_OK)
-def health():
+app = create_app()
+
+
+class HealthCheckResponse(BaseModel):
+    status: str = Field(..., example="ok")
+
+
+@app.get("/healthcheck", status_code=status.HTTP_200_OK, response_model=HealthCheckResponse)
+def healthcheck():
     return {"status": "ok"}
