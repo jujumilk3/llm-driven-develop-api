1f3c1ab13880519131fbd755c9664548b2f3f3e3
jujumilk3 <jujumilk3@gmail.com>
2023-05-30 23:45:48 +0900

    save data as json

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 class BaseConfigs:
     APP_ROOT_DIR: str = Path(__file__).parent.parent.parent
+    COLLECTIONS_DIR: str = os.path.join(APP_ROOT_DIR, "app/collections")
     APP_NAME: str = "LDD API"
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     OPENAI_ORG_KEY: str = os.getenv("OPENAI_ORG_KEY")
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,11 @@
+import json
+import os
+from typing import Union
+
 from marvin import ai_fn
 from pydantic import BaseModel, Field
+
+from app.core.config import configs
 
 
 class UserModel(BaseModel):
@@ -23,6 +29,16 @@ def create_user(user_model: UserModel) -> dict[str, str]:
     """
 
 
+def save_data_as_json(data: Union[dict, list], collection_name: str, file_name: str):
+    # create dir if not exists
+    target_dir: str = f"{configs.COLLECTIONS_DIR}/{collection_name}/"
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    with open(f"{target_dir}/{file_name}.json", "w") as f:
+        f.write(json.dumps(data))
+
+
 if __name__ == "__main__":
     # print(list_fruits(3))
     # NOTE: Date 2023-05-30 It returned ['apple', 'banana', 'orange']
@@ -34,4 +50,9 @@ if __name__ == "__main__":
     )
     created_user = create_user(user_model)
     print(created_user)
+    save_data_as_json(
+        data=created_user,
+        collection_name="users",
+        file_name=f"user_{created_user['user_token']}",
+    )
     # NOTE: DATE 2023-05-30 It returned {'user_token': 'user_token', 'nickname': 'nickname', 'email': 'email@email.com', 'password': '5f4dcc3b5aa765d61d8327deb882cf99'}
